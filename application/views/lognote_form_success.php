ced866cbc2858994e6cc682872c2a87f571fc00f
akhiladamsari <akhiladamsari@gmail.com>
2016-12-14 23:46:27 +0530

    "lognote update successfull"

---
+++ application/views/lognote_form_success.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+    <!-- Title -->
+    <title>Bird Log Book</title>
+    <!-- Meta -->
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <!-- Favicon -->
+    <link href="favicon.ico" rel="shortcut icon">
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.css" rel="stylesheet">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/animate.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/nexus.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/custom.css" rel="stylesheet">
+    <!-- Google Fonts-->
+    <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300" rel="stylesheet" type="text/css">
+    <style type="text/css">
+    </style>
+</head>
+<body>
+<div id="body-bg">
+    <!-- Header -->
+    <div id="header">
+        <div class="container">
+            <div class="row">
+                <!-- Logo -->
+                <div class="logo">
+                    <a href="index.html">
+                        <img src="<?php echo base_url(); ?>img/applogo.png" alt="Logo"  height="100"  >                            </a>
+                </div>
+                <!-- End Logo -->
+            </div>
+        </div>
+    </div>
+    <!-- End Header -->
+    <!-- Top Menu -->
+    <div id="hornav" class="bottom-border-shadow">
+        <div class="container no-padding border-bottom">
+            <div class="row">
+                <!-- <div class="col-md-8 no-padding"> -->
+                <div class="visible-lg">
+                    <ul id="hornavmenu" class="nav navbar-nav">
+                        <li>
+                            <a href="index.html" class="fa-home active">Home</a>
+                        </li>
+                        <li>
+
+                            <a href="<?php echo base_url(); ?>page_nav/goto_bird_glossary" class="fa-gears ">Bird Glossary</a>
+                        <li>
+                            <a href="<?php echo base_url(); ?>page_nav/goto_map" class="fa-home active">Map</a>
+                        </li>
+                        <li>
+                            <span class="fa-font ">Log Note Library</span>
+                            <ul>
+                                <li>
+                                    <a href="<?php echo base_url(); ?>page_nav/goto_my_log_notes">My Log Notes</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url(); ?>page_nav/goto_shared_lognotes">Shared Log Notes</a>
+                                </li>
+                            </ul>
+                        </li><li>
+                            <a href="<?php echo base_url(); ?>page_nav/goto_gallery" class="fa-th ">Gallery</a>
+
+                        </li>
+                        <li>
+                            <a href="<?php echo base_url(); ?>page_nav/goto_contact" class="fa-comment ">Contact Us</a>
+                        </li>
+                        <div class="login btn padding-horiz-20">
+                            <button type="button" class="btn btn-success btn-sm"><a href="<?php echo base_url(); ?>page_nav/goto_login">Log In</a></button>
+                            <button type="button" class="btn btn-success btn-sm"><a href="<?php echo base_url(); ?>page_nav/goto_signup">Sign Up</a></button>
+                        </div>
+
+                    </ul>
+                </div>
+
+            </div>
+            <!-- </div> -->
+        </div>
+    </div>
+    <!-- End Top Menu -->
+    <!-- === END HEADER === -->
+    <!-- === BEGIN CONTENT === -->
+
+
+    <div class="container background-white">
+
+        <div class="row margin-vert-30">
+            <!-- Main Column -->
+            <div class="col-md-9">
+                <!-- Main Content -->
+                <div class="headline">
+                    <h2>Log Note</h2>
+                </div>
+                <!-- Contact Form -->
+                <form class="login-page">
+                    <p><?php echo $message ?></p>
+                </form>
+                <!-- End Contact Form -->
+                <!-- End Main Content -->
+            </div>
+            <!-- End Main Column -->
+
+        </div>
+    </div>
+
+    <!-- === END CONTENT === -->
+    <!-- === BEGIN FOOTER === -->
+    <div id="footer" class="background-grey">
+        <div class="container">
+            <div class="row">
+                <!-- Footer Menu -->
+                <div id="footermenu" class="col-md-8">
+                    <ul class="list-unstyled list-inline">
+                        <li>
+                            <a href="#" target="_blank">Sample Link</a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">Sample Link</a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">Sample Link</a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">Sample Link</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- End Footer Menu -->
+                <!-- Copyright -->
+                <div id="copyright" class="col-md-4">
+                    <p class="pull-right">(c) 2014 Your Copyright Info</p>
+                </div>
+                <!-- End Copyright -->
+            </div>
+        </div>
+    </div>
+    <!-- End Footer -->
+    <!-- JS -->
+    <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>js/bootstrap.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>js/scripts.js"></script>
+    <!-- Isotope - Portfolio Sorting -->
+    <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.isotope.js" type="text/javascript"></script>
+    <!-- Mobile Menu - Slicknav -->
+    <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.slicknav.js" type="text/javascript"></script>
+    <!-- Animate on Scroll-->
+    <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.visible.js" charset="utf-8"></script>
+    <!-- Sticky Div -->
+    <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.sticky.js" charset="utf-8"></script>
+    <!-- Slimbox2-->
+    <script type="text/javascript" src="<?php echo base_url(); ?>js/slimbox2.js" charset="utf-8"></script>
+    <!-- Modernizr -->
+    <script src="<?php echo base_url(); ?>js/modernizr.custom.js" type="text/javascript"></script>
+    <!-- End JS -->
+</body>
+</html>
+<!-- === END FOOTER ===-->
---
+++ application/views/new.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Akhila Damsari
+ * Date: 12/14/2016
+ * Time: 11:45 PM
+ */
