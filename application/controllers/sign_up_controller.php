78bf99d7a55fbb155a127c3d3d902ed1cc25bbb6
Ishan <ishanweragoda@gmail.com>
2017-01-14 10:40:15 +0530

    dashbord
    fg

--- application/controllers/sign_up_controller.php
+++ application/controllers/sign_up_controller.php
@@ -70,4 +70,201 @@ class sign_up_controller extends CI_Controller
         $this->session->sess_destroy();
         $this->load->view('home');
     }
+    public function adminlogin()
+    {
+        //$result = new sign_up_controller().count_users();
+        $result=$this->count_new_users();
+        $result2=$this->count_new_comments();
+        $result3=$this->count_new_lognotes();
+        $result4=$this->count_new_complains();
+        $data['users'] = $result;
+        $data['comments'] = $result2;
+        $data['lognotes'] = $result3;
+        $data['complains'] = $result4;
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+        $this->load->model('users_model');
+        $result = $this->users_model->admin_login($username,$password);
+
+        if ($result == TRUE) {
+
+            $data['username'] = $this->input->post('username');
+            $data['logged_in'] = TRUE;
+
+            $this->session->set_userdata($data);
+            if($this->session->has_userdata('username')) {
+                $this->load->view('admin_dashboard',$data);
+            }else {
+                $this->load->view('adminlogin');
+            }
+        } else {
+            $this->load->view('adminlogin');
+        }
+        return $data;
+    }
+
+
+   
+    public function count_new_users(){
+        $this->load->model('users_model');
+        $count=$this->users_model->count_new_users();
+        return $count;
+        //  $this->users_model->count_new_users();
+    }
+    public function count_new_comments(){
+        $this->load->model('users_model');
+        $count=$this->users_model->count_new_comments();
+        return $count;
+        //  $this->users_model->count_new_users();
+    }
+    public function count_new_lognotes(){
+        $this->load->model('users_model');
+        $count=$this->users_model->count_new_lognotes();
+        return $count;
+        //  $this->users_model->count_new_users();
+    }
+    public function count_new_complains(){
+        $this->load->model('users_model');
+        $count=$this->users_model->count_new_complains();
+        return $count;
+        //  $this->users_model->count_new_users();
+    }
+    public function select_new_users()
+    {
+        //$da = $this->adminlogin();
+        if ($this->session->has_userdata('username')) {
+            $result = $this->count_new_users();
+            $result2 = $this->count_new_comments();
+            $result3 = $this->count_new_lognotes();
+            $result4 = $this->count_new_complains();
+            $data['users'] = $result;
+            $data['comments'] = $result2;
+            $data['lognotes'] = $result3;
+            $data['complains'] = $result4;
+            //$result4 = $this->adminlogin();
+
+            $this->load->model('users_model');
+
+            $data['userdetail'] = $this->users_model->select_new_users();
+            $this->load->view('admin_dashboard', $data);
+
+            //  $this->users_model->count_new_users();
+        }else{
+            $this->load->view('adminlogin');
+        }
+    }
+    public function updateview(){
+        $data_id = $this->uri->segment(3);
+        $this->load->model('users_model');
+        $this->users_model->viewed($data_id);
+        $this->select_new_users();
+        //$this->load->view('admin_dashboard',$data);
+    }
+
+    public function updateapprove(){
+        $data_id = $this->uri->segment(3);
+        $this->load->model('users_model');
+        $this->users_model->approved($data_id);
+        $this->select_new_users();
+    }
+
+    public function remove_user()
+    {
+        //$da = $this->adminlogin();
+        if ($this->session->has_userdata('username')) {
+
+            $this->load->model('users_model');
+            $data['userdetail']=NULL;
+            $data['approved_user_details'] = $this->users_model->select_users();
+
+            $this->load->view('admin_dashboard', $data);
+
+            //  $this->users_model->count_new_users();
+        }else{
+            $this->load->view('adminlogin');
+        }
+    }
+    public function deleteuser(){
+        $data_id = $this->uri->segment(3);
+        $this->load->model('users_model');
+        $this->users_model->deleteuser($data_id);
+        $this->remove_user();
+        //$this->load->view('admin_dashboard',$data);
+    }
+
+    public function select_new_comment()
+    {
+        //$da = $this->adminlogin();
+        if ($this->session->has_userdata('username')) {
+            $result = $this->count_new_users();
+            $result2 = $this->count_new_comments();
+            $result3 = $this->count_new_lognotes();
+            $result4 = $this->count_new_complains();
+            $data['users'] = $result;
+            $data['comments'] = $result2;
+            $data['lognotes'] = $result3;
+            $data['complains'] = $result4;
+            //$result4 = $this->adminlogin();
+
+            $this->load->model('users_model');
+            $data['userdetail']=NULL;
+            $data['commentdetail'] = $this->users_model->select_new_comments();
+            $this->load->view('admin_dashboard', $data);
+
+            //  $this->users_model->count_new_users();
+        }else{
+            $this->load->view('adminlogin');
+        }
+    }
+    public function update_viewed_comment(){
+        $data_id = $this->uri->segment(3);
+        $this->load->model('users_model');
+        $this->users_model->rejectcomment($data_id);
+        $this->select_new_comment();
+        //$this->load->view('admin_dashboard',$data);
+    }
+
+    public function update_approve_comment(){
+        $data_id = $this->uri->segment(3);
+        $this->load->model('users_model');
+        $this->users_model->approvedcomment($data_id);
+        $this->select_new_comment();
+    }
+
+
+
+    public function select_new_complains()
+    {
+        //$da = $this->adminlogin();
+        if ($this->session->has_userdata('username')) {
+            $result = $this->count_new_users();
+            $result2 = $this->count_new_comments();
+            $result3 = $this->count_new_lognotes();
+            $result4 = $this->count_new_complains();
+            $data['users'] = $result;
+            $data['comments'] = $result2;
+            $data['lognotes'] = $result3;
+            $data['complains'] = $result4;
+            //$result4 = $this->adminlogin();
+
+            $this->load->model('users_model');
+            $data['userdetail']=NULL;
+            $data['complainsdetail'] = $this->users_model->select_new_complains();
+            $this->load->view('admin_dashboard', $data);
+
+            //  $this->users_model->count_new_users();
+        }else{
+            $this->load->view('adminlogin');
+        }
+    }
+    public function update_viewed_complains(){
+        $data_id = $this->uri->segment(3);
+        $this->load->model('users_model');
+        $this->users_model->viewedcomplain($data_id);
+        $this->select_new_complains();
+        //$this->load->view('admin_dashboard',$data);
+    }
+    public function select_new_lognotes(){
+
+    }
 }
